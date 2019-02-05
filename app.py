59ba0c4515377994f6664fb5fba0cd6267d51e40
Ahmed Nergal <ahmed.nergal@gmail.com>
2019-02-05 20:24:43 +0200

    Modified app.py and added .gitignore file

--- app.py
+++ app.py
@@ -1,3 +1,102 @@
+import math
+
+
+numbers = []                    # Number of line in the original srt file
+timestamps = []                 # Timestamps line in the original srt file
+content = []                    # Subtitle content line in the original srt file
+separate_timestamps = []        # Separated timestamps list
+dict_timestamps = []            # Dictionary of original srt timestamps separated into "hr", "min", "sec", "ms"
+shifted_timestamps = []         # Dictionary of shifted timestamps separated into "hr", "min", "sec", "ms"
+starter_timestamp_lines = []    # List of all lines beginning timestamps
+ender_timestamp_lines = []      # List of all lines ending timestamps
+
+def srt_line_parser(lines):
+    try:
+        for line in lines:
+            numbers.append(line.split("\n")[0])
+            timestamps.append(line.split("\n")[1])
+            content.append(line.split("\n")[2:])
+    except IndexError:
+        pass
+
+    try:
+        for i in range(len(numbers)):
+            print(numbers[i])
+            print(timestamps[i])
+            print(content[i])
+            print("\n", end="")
+
+    except IndexError:
+        pass
+
+def timestamp_separator(timestamps):
+    for timestamp in timestamps:
+        before = timestamp.split(" -")[0]
+        after = timestamp.split("> ")[1]
+
+        separate_timestamps.append(before)
+        separate_timestamps.append(after)
+
+def timestamp_dict_creator(separate_timestamps):
+    for stamp in separate_timestamps:
+        dictionary = {
+            "hr": int(stamp.split(":")[0]),
+            "min": int(stamp.split(":")[1]),
+            "sec": int(stamp.split(":")[2].split(",")[0]),
+            "ms": int(stamp.split(":")[2].split(",")[1])
+        }
+
+        dict_timestamps.append(dictionary)
+
+
+def shift_creator(shift_direction, shift_value):
+    hr = 0
+    min = 0
+    sec = 0
+    ms = 0
+    shift_dict = {
+        "hr": 0,
+        "min": 0,
+        "sec": 0,
+        "ms": 0
+    }
+
+    if shift_value < 0.999:
+        ms = shift_value * 1000
+
+    else:
+        sec = int(math.modf(shift_value)[1])
+        ms = int(math.ceil(1000 * math.modf(shift_value)[0]))
+
+        if sec >= 60:
+            min = int(sec / 60)
+            sec = sec - (min * 60)
+
+        if min >= 60:
+            hr = int(min / 60)
+            min = min - (hr * 60)
+
+    if shift_direction == "B" or shift_direction == "b":
+        shift_dict = {
+            "hr": hr * -1,
+            "min": min * -1,
+            "sec": sec * -1,
+            "ms": ms * -1
+        }
+
+    elif shift_direction == "F" or shift_direction == "f":
+        shift_dict = {
+            "hr": hr,
+            "min": min,
+            "sec": sec,
+            "ms": ms
+        }
+    
+    else:
+        return "Invalid Input" 
+
+    return shift_dict
+
 def sub_modifier(current, shift):
     altered_hr = current["hr"] + shift["hr"]
     altered_min = current["min"] + shift["min"]
@@ -37,80 +136,6 @@ def sub_modifier(current, shift):
 
     return altered_timestamp
 
-srt_file = open("Sukiyaki Western Django (2007).srt", "r").read()
-modified_srt_file = open("modified srt.srt", "w")
-
-lines = srt_file.split("\n\n")
-"""
-current = {
-    "hr": 0,
-    "min": 55,
-    "sec": 50,
-    "ms": 990
-}
-"""
-shift = {
-    "hr": 0,
-    "min": 0,
-    "sec": 1,
-    "ms": 1
-}
-
-
-numbers = []
-timestamps = []
-content = []
-
-try:
-    for line in lines:
-        numbers.append(line.split("\n")[0])
-        timestamps.append(line.split("\n")[1])
-        content.append(line.split("\n")[2:])
-except IndexError:
-    pass
-
-try:
-    for i in range(len(numbers)):
-        print(numbers[i])
-        print(timestamps[i])
-        print(content[i])
-        print("\n", end="")
-
-except IndexError:
-    pass
-
-separate_timestamps = []
-for timestamp in timestamps:
-    before = timestamp.split(" -")[0]
-    after = timestamp.split("> ")[1]
-
-    separate_timestamps.append(before)
-    separate_timestamps.append(after)
-
-print(separate_timestamps)
-
-dict_timestamps = []
-
-for stamp in separate_timestamps:
-    dictionary = {
-        "hr": int(stamp.split(":")[0]),
-        "min": int(stamp.split(":")[1]),
-        "sec": int(stamp.split(":")[2].split(",")[0]),
-        "ms": int(stamp.split(":")[2].split(",")[1])
-    }
-
-    dict_timestamps.append(dictionary)
-
-#print(split_timestamps)
-
-shifted_timestamps = []
-
-for split_timestamp in dict_timestamps:
-    modified_stamp = sub_modifier(split_timestamp, shift)
-    shifted_timestamps.append(modified_stamp)
-
-starter_timestamp_lines = []
-ender_timestamp_lines = []
 def timestamp_formatter(shifted_timestamps):
     for mod_stamp in shifted_timestamps:
         if shifted_timestamps.index(mod_stamp) % 2 == 0:
@@ -121,17 +146,46 @@ def timestamp_formatter(shifted_timestamps):
             ender_timestamp_lines.append(mod_lin)
     return (starter_timestamp_lines, ender_timestamp_lines)
 
-timestamp_formatter(shifted_timestamps)
-print(ender_timestamp_lines)
+def output_srt_file_creator(lines):
+    try:
+        for i in range(len(lines)):
+                modified_srt_file.write(lines[i].split("\n")[0] + "\n")
+                modified_srt_file.write(starter_timestamp_lines[i])
+                modified_srt_file.write(ender_timestamp_lines[i])
+                for sub_line in content[i]:
+                    modified_srt_file.write(sub_line)
+                    modified_srt_file.write("\n")
+                modified_srt_file.write("\n")
+    except IndexError:
+        pass
+
+
+# User Interface
+
+file_name = input("Enter File Name: ")
+shift_direction = input("Shift Direction (Enter \"F\" or \"f\" for forward shifting or enter \"B\" or \"b\" for backward shifting: ")
+shift_value = float(input("Enter shift value in seconds.millisecions format: "))
 
 try:
-    for i in range(len(lines)):
-            modified_srt_file.write(lines[i].split("\n")[0] + "\n")
-            modified_srt_file.write(starter_timestamp_lines[i])
-            modified_srt_file.write(ender_timestamp_lines[i])
-            for sub_line in content[i]:
-                modified_srt_file.write(sub_line)
-                modified_srt_file.write("\n")
-            modified_srt_file.write("\n")
+    if file_name.split(".")[1] == "srt":
+        srt_file = open(file_name, "r").read()
+        modified_srt_file = open(file_name.split(".")[0] + " modified.srt", "w")
+        lines = srt_file.split("\n\n")
+        srt_line_parser(lines)
+        timestamp_separator(timestamps)
+        timestamp_dict_creator(separate_timestamps)
+        shift = shift_creator(shift_direction, shift_value)
+
+
+        for split_timestamp in dict_timestamps:
+            modified_stamp = sub_modifier(split_timestamp, shift)
+            shifted_timestamps.append(modified_stamp)
+
+        timestamp_formatter(shifted_timestamps)
+        output_srt_file_creator(lines)
+    else:
+        print("Format Doesn't Match .srt")
+except FileNotFoundError:
+    print("File Doesn't Exist")
 except IndexError:
-    pass
+    print("Invalid Input")
